d3972fc55fb23a34381d82a75590cb1f5db15907
Soe Than <soethan@gmail.com>
2017-06-06 22:10:00 +0800

    initial checkin

---
+++ LinqTest/LinqTest/MockData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    public class MockData
+    {
+        public static List<Company> GetCompanies()
+        {
+            var list = new List<Company> {
+                new Company { CompanyId = 1, CompanyName = "Company 1" },
+                new Company { CompanyId = 2, CompanyName = "Company 2" },
+                new Company { CompanyId = 3, CompanyName = "Company 3" }
+            };
+            return list;
+        }
+
+        public static List<Employee> GetEmployees()
+        {
+            var list = new List<Employee>
+            {
+                new Employee { EmployeeId = 1, EmployeeName = "Employee 1", CompanyId = 1 },
+                new Employee { EmployeeId = 2, EmployeeName = "Employee 2", CompanyId = 2 },
+                new Employee { EmployeeId = 3, EmployeeName = "Employee 3", CompanyId = 2 },
+                new Employee { EmployeeId = 4, EmployeeName = "Employee 4" }
+            };
+            return list;
+        }
+    }
+}
---
+++ LinqTest/LinqTest/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/linq/join-by-using-composite-keys
+    /// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/join-clause
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Company> companyList = MockData.GetCompanies();
+            List<Employee> employeeList = MockData.GetEmployees();
+
+            #region Left Outer Join
+
+            var leftOuterJoinQuery = employeeList
+                                        .GroupJoin(
+                                            companyList,
+                                            emp => emp.CompanyId,
+                                            company => company.CompanyId,
+                                            (emp, cmps) => new { Employee = emp, Companies = cmps }
+                                        ).SelectMany(
+                                            empComps => empComps.Companies.DefaultIfEmpty(),
+                                            (empCompsR, cmpR) => new
+                                            {
+                                                EmployeeId = empCompsR.Employee.EmployeeId,
+                                                EmployeeName = empCompsR.Employee.EmployeeName,
+                                                CompanyId = cmpR == null ? string.Empty : cmpR.CompanyId.ToString(),
+                                                CompanyName = cmpR == null ? string.Empty : cmpR.CompanyName
+                                            }
+                                        );
+
+            Console.WriteLine("************** Left Outer Join **************");
+            foreach (var item in leftOuterJoinQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            #region Right Outer Join
+
+            var rightOuterJoinQuery = companyList
+                                        .GroupJoin(
+                                            employeeList,
+                                            company => company.CompanyId,
+                                            emp => emp.CompanyId,
+                                            (cmp, emps) => new { Company = cmp, Employees = emps }
+                                        ).SelectMany(
+                                            compEmps => compEmps.Employees.DefaultIfEmpty(),
+                                            (compEmpsR, empR) => new
+                                            {
+                                                CompanyId = compEmpsR.Company.CompanyId,
+                                                CompanyName = compEmpsR.Company.CompanyName,
+                                                EmployeeId = empR == null ? string.Empty : empR.EmployeeId.ToString(),
+                                                EmployeeName = empR == null ? string.Empty : empR.EmployeeName
+                                            }
+                                        );
+
+            Console.WriteLine("************** Right Outer Join **************");
+            foreach (var item in rightOuterJoinQuery)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            #region GroupBy and GroupBy...Having
+
+            Console.WriteLine("************** GroupBy and GroupBy...Having **************");
+            var employeesCountByCompany = rightOuterJoinQuery
+                                                .GroupBy(e => e.CompanyId)
+                //.Where(empGroup => empGroup.Count() > 1)
+                                                .Select(g =>
+                                                    new
+                                                    {
+                                                        CompanyId = g.Key,
+                                                        TotalEmployees = g.Count()
+                                                    }
+                                                );
+
+            foreach (var item in employeesCountByCompany)
+            {
+                Console.WriteLine(string.Format("CompanyId:{0};TotalEmployees={1};", item.CompanyId, item.TotalEmployees));
+            }
+
+            #endregion
+
+            #region Left Outer Join another example
+
+            var grpJoin = from e in employeeList
+                          join c in companyList
+                          on e.CompanyId equals c.CompanyId
+                          into cmpGroup
+                          select new
+                          {
+                              e.EmployeeId,
+                              e.EmployeeName,
+                              //cmpGroup,
+                              CompanyId = cmpGroup.Any() ? cmpGroup.FirstOrDefault().CompanyId.Value.ToString() : "",
+                              CompanyName = cmpGroup.Any() ? cmpGroup.FirstOrDefault().CompanyName : ""
+                          };
+
+            foreach (var item in grpJoin)
+            {
+                Console.WriteLine(string.Format("EmployeeId:{0};EmployeeName={1};CompanyId={2};CompanyName={3};", item.EmployeeId, item.EmployeeName, item.CompanyId, item.CompanyName));//item.cmpGroup
+            }
+
+            #endregion
+            
+            Console.Read();
+        }
+    }
+}
